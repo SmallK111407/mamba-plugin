47ae11ec0b855dd151af28d333b2aef835190948
SmallK111407 <3399280843@qq.com>
2024-04-30 00:39:07 +0800

    细节优化

--- apps/globalBindHelp.js
+++ apps/globalBindHelp.js
@@ -23,7 +23,7 @@ export class globalBindHelp extends plugin {
     const messages = [(
       '1.首先选择任意渠道下载软件【国际服一键获取Stoken.apk】\n',
       '123盘: https://www.123pan.com/s/6QMTjv-w0L0h.html\n',
-      '蓝奏云盘: https://wwl.lanzoum.com/iJfOF1x21ave 密码:gycb\n\n'
+      '蓝奏云盘: https://wwl.lanzoum.com/iJfOF1x21ave 密码:gycb'
     ), (
       '2.下载完后打开代理后进入此软件', segment.image(`${_path}/resources/globalBindHelp/1.png`),
       `\n随后输入自己的邮箱账号密码并登录`
